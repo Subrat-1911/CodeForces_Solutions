047e290a99a1dad70992fbc2d780d7519c4c1318
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-08-27 01:35:44 +0530

    Auto-sync: Code for 281A. Word Capitalization

--- 281A.word-capitalization/281A.word-capitalization.cpp
+++ 281A.word-capitalization/281A.word-capitalization.cpp
@@ -1,11 +1,26 @@
 #include<iostream>
 #include<string>
 using namespace std;
-int main(){
-    string s;
-    cin>>s;
-    if(s[0]>'a' && s[0]<'z'){
-        s[0]-=32;
+bool func(long long nums){
+    int sev=0;
+    int four=0;
+    int count=0;
+    while(nums>0){
+        int x=nums%10;
+        if(x==4) four++;
+        else if(x==7) sev++;
+        count++;
+        nums/=10;
     }
-    cout<<s;
+    int sum=four+sev;
+    if(sum==4 || sum==7) return true;
+    else return false;
+    
+}
+int main(){
+    long long nums;
+    cin>>nums;
+    int x=func(nums);
+    if(x==0) cout<<"NO";
+    else cout<<"YES";
 }
