e7bdf08c75b84431e867d98ef701b92e3c339481
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-08-26 22:30:03 +0530

    Auto-sync: Code for 275A. Lights Out

--- 275A.lights-out/275A.lights-out.cpp
+++ 275A.lights-out/275A.lights-out.cpp
@@ -14,14 +14,14 @@ int main(){
             if(arr[i][j]%2==0) continue;
             else{
                 ans[i][j]=1-ans[i][j];  
-                if(i-1>0){ //up.
+                if(i-1>=0){ //up.
                     // if 1 convert into 0, if 0 convert into one
                     ans[i-1][j]=1-ans[i-1][j];
                 }
                 if(i+1<3){  // down
                     ans[i+1][j]=1-ans[i+1][j];
                 }
-                if(j-1>0){ //left
+                if(j-1>=0){ //left
                     ans[i][j-1]=1-ans[i][j-1];
                 }
                 if(j+1<3){ //right
