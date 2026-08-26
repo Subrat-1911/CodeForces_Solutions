d78481da7e0db489059bc649f81ab0b20efc3104
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-08-27 01:16:22 +0530

    Auto-sync: Code for 110A. Nearly Lucky Number

--- 110A.nearly-lucky-number/110A.nearly-lucky-number.cpp
+++ 110A.nearly-lucky-number/110A.nearly-lucky-number.cpp
@@ -3,15 +3,17 @@
 using namespace std;
 bool func(int nums){
     int count=0;
+    bool flag = false;
     while(nums>0){
         int x=nums%10;
-        if(x!=4 && x!=7){
-            return false;
+        if(x==4 || x==7){
+            flag= true;
         }
+        else flag = false;
         count++;
         nums/=10;
     }
-    if(count==4 || count ==7){
+    if((count==4 || count ==7) && flag == true){
         return true;
 
     }
