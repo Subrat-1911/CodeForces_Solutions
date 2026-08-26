b047f032c2f223d1b3fed22995034ea553e0aab9
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-08-27 00:44:39 +0530

    Auto-sync: Code for 59A. Word

--- 59A.word/59A.word.cpp
+++ 59A.word/59A.word.cpp
@@ -4,31 +4,8 @@ using namespace std;
 int main(){
     string s;
     cin>>s;
-    int upp=0;
-    int low=0;
-    int n=s.size();
-    for(int i=0;i<n;i++){  //'a' is 97,'A' is 65.
-        if(s[i]>='a'){
-            low++;
-        }
-        else if(s[i]>='A'){
-            upp++;
-
-        }
-    }
-    if(low>=upp){  // lower case
-        for(int i=0;i<n;i++){
-            if(s[i] >= 'A' && s[i] <= 'Z'){
-                s[i]+=32;
-            }
-        }
-    }
-    else{ //upercase
-        for(int i=0;i<n;i++){
-            if(s[i] >= 'a' && s[i] <= 'z'){
-                s[i]-=32;
-            }
-        }
+    if(s[0]>='a' && s[0]<='z'){
+        s[0]-=32;
     }
     cout<<s;
 }
