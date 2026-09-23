7dabcb7134225ed533ed0cb458b81631b5e30da4
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-09-23 07:14:11 +0530

    Auto-sync: Code for 1472A. Cards for Friends

--- 1472A.cards-for-friends/1472A.cards-for-friends.cpp
+++ 1472A.cards-for-friends/1472A.cards-for-friends.cpp
@@ -1,1 +1,27 @@
-#include<iostream>using namespace std;int main(){    int t;    cin>>t;    for(int i=0;i<t;i++){        int count=1;        int w,h,n;        cin>>w>>h>>n;        while(w%2==0){            w=w/2;            count*=2;        }        while(h%2==0){            h=h/2;            count*=2;        }        if(count>=n){            cout<<"YES"<<endl;        }        else{            cout<<"NO"<<endl;        }    }        }
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        int count=1;
+        int w,h,n;
+        cin>>w>>h>>n;
+        while(w%2==0){
+            w=w/2;
+            count*=2;
+        }
+        while(h%2==0){
+            h=h/2;
+            count*=2;
+        }
+        if(count>=n){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+    
+    
+}
