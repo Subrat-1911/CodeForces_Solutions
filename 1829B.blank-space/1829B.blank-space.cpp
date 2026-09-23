041801305f61204179e10cc0df3ba44091c15552
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-09-23 07:36:57 +0530

    Auto-sync: Code for 1829B. Blank Space

--- 1829B.blank-space/1829B.blank-space.cpp
+++ 1829B.blank-space/1829B.blank-space.cpp
@@ -1,1 +1,32 @@
-N/A
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    
+    for(int i=0;i<t;i++){
+        int n;
+        cin>>n;
+        int nums[n];
+        for(int j=0;j<n;j++){
+            cin>>nums[j];
+        }
+        int countZeros=0;
+        int maxZeros=0;
+        for(int k=0;k<n;k++){
+            if(nums[k]==0){
+                countZeros++;
+                if(maxZeros<countZeros){
+                    maxZeros=countZeros;
+                }
+            }
+            else{
+                countZeros=0;
+            }
+        }
+        cout<<maxZeros<<endl;
+        
+
+    }
+    
+}
