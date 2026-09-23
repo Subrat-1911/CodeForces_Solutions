50588e24875cd5b018f658837ae0f3964f4bb7b4
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-09-23 07:13:47 +0530

    Auto-sync: Code for 231A. Team

---
+++ 231A.team/231A.team.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int count=0;
+    for(int i=0;i<n;i++){
+        int x;
+        int y;
+        int z;
+        cin>>x;
+        cin>>y;
+        cin>>z;
+        if(x+y+z>1){
+            count++;
+        }
+    }
+    cout<<count;
+    
+}
