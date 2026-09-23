9965b8dbcaa01a5d70db6ce72d23acfd16ab7cb9
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-09-23 07:13:43 +0530

    Auto-sync: Code for 71A. Way Too Long Words

---
+++ 71A.way-too-long-words/71A.way-too-long-words.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        string s;
+        cin>>s;
+        int size= s.size();
+        if(size<=10){
+            cout<<s<<endl;
+        }
+        else{
+            cout<<s[0]<<size-2<<s[size-1]<<endl;
+        }
+    }
+    
+}
