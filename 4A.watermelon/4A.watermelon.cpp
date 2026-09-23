7de6e5873dcd9d6c852d46af597f2918cee56612
Subrat Sahoo <subratsahoo28032006@gmail.com>
2026-09-23 07:12:43 +0530

    Auto-sync: Code for 4A. Watermelon

--- 4A.watermelon/4A.watermelon.cpp
+++ 4A.watermelon/4A.watermelon.cpp
@@ -2,25 +2,15 @@
 
 using namespace std;
 
-void solveWatermelon() {
-    int weight;
-    if (cin >> weight) {
-        // Bitwise AND use kiya hai even check karne ke liye
-        // aur weight 2 se bada hona chahiye
-        if (weight > 2 && (weight & 1) == 0) {
+// Fresh Sync Run - Watermelon Problem
+int main() {
+    int w;
+    if (cin >> w) {
+        if (w > 2 && w % 2 == 0) {
             cout << "YES\n";
         } else {
             cout << "NO\n";
         }
     }
-}
-
-int main() {
-    // Fast I/O taaki execution time alag aaye
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-    solveWatermelon();
-    
     return 0;
 }
